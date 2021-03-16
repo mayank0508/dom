0ee6ad9fe6e481187ffb14f08533a0a843b6496c
mayank0508 <thakurmayank0508@gmail.com>
2021-03-16 23:42:14 +0530

    token

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const ul = document.body.firstElementChild.nextElementSibling;
+// const ul = document.body.firstElementChild.nextElementSibling;
 
-const li1 = ul.firstElementChild;
+// const li1 = ul.firstElementChild;
 
-console.log(ul);
-console.log(li1);
+// console.log(ul);
+// console.log(li1);
+
+const ul = document.querySelector('section');
+const button = document.querySelector('button');
+
+// ul.style.backgroundColor = 'red'
+
+ul.className = 'red-bg';
+
+button.addEventListener('click', () => {
+    // if (ul.className === 'red-bg visible') {
+    //     ul.className = 'red-bg invisible';
+    // } else {
+    //     ul.className = 'red-bg visible';
+    // }
+
+    // ul.classList.toggle('visible');
+    ul.classList.toggle('invisible');
+});
